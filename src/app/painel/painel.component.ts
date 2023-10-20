33bb602831acc8012fd3b8e8f74caedbaca7ffe4
Talita Santos <talita-s@Talitas-MacBook-Pro.local>
2023-10-20 14:33:13 -0300

    build: Projeto atualizado para Angular CLI 16.2.7

---
+++ src/app/painel/painel.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+import { Frase } from '../shared/frase.model';
+import { FRASES } from './frases-mock';
+
+@Component({
+  selector: 'app-painel',
+  templateUrl: './painel.component.html',
+  styleUrls: ['./painel.component.css'],
+})
+export class PainelComponent {
+  public frases: Frase[] = FRASES;
+  public instrucao: string = 'Traduza a frase:';
+
+  atualizaResposta(): void {
+    console.log('teste');
+  }
+}
