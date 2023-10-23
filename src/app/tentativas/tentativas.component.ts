e6d39788814f8c4b3c88cff6641f97b38764b775
Talita-S <talitassantos2015@gmail.com>
2023-10-23 09:24:06 -0300

    feat: Componente Tentativas: Exibindo corações

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 
+import { Coracao } from '../shared/coracao.model';
+
 @Component({
   selector: 'app-tentativas',
   templateUrl: './tentativas.component.html',
-  styleUrls: ['./tentativas.component.css']
+  styleUrls: ['./tentativas.component.css'],
 })
 export class TentativasComponent {
-  public coracaoVazio: string = "/assets/coracao_vazio.png";
-  public coracaoCheio: string = "/assets/coracao_cheio.png";
+  public coracoes: Coracao[] = [
+    new Coracao(true),
+    new Coracao(true),
+    new Coracao(true),
+  ];
 }
